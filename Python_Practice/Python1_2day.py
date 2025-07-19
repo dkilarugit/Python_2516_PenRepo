11c5dd1de9ccffb80a42267bb89b33f8002597cf
Durga Narasaiah Naidu Kilaru <dnnaidukilaru@gmail.com>
2025-07-19 18:40:40 +0530

    Lists mutable vs Immutable

--- Python_Practice/Python1_2day.py
+++ Python_Practice/Python1_2day.py
@@ -7,9 +7,9 @@ print("List 2:", list_2)
 print(id(list_1))
 print(id(list_2))
 
-Output:
+# Output:
 # Both lists refer to the same object or id
-List 1: [1, 2, 3, 4]
-List 2: [1, 2, 3, 4]
-1992913721600
-1992913721600
+# List 1: [1, 2, 3, 4]
+# List 2: [1, 2, 3, 4]
+# 1992913721600
+# 1992913721600
